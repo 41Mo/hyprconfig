f89adb431bf7033e5f8d1d21879a22eda3e5a18f
Alex Molchanov <amol4anov@mail.ru>
2025-02-10 16:07:34 +0300

    shared configs for desktop and laptop

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # set -e -x
 
 HYPERLAND_CONFIG_DIR="$HOME/.config/hypr"
@@ -7,11 +6,27 @@ WAYBAR_CONFIG_DIR="$HOME/.config/waybar"
 
 SCRIPT=$(realpath -s "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+HYPR_SHARED_PLATFORM_CONF="empty"
 
 if [ ! -n "$XDG_CONFIG_HOME" ]; then
   echo "Error: XDG_CONFIG_HOME not set";
   exit 1;
 fi
+
+if [ "$1" == "--desktop" ]; then
+  HYPR_SHARED_PLATFORM_CONF="$SCRIPTPATH/hypr/desktop.conf"
+fi
+
+if [ "$1" == "--laptop" ]; then
+  HYPR_SHARED_PLATFORM_CONF="$SCRIPTPATH/hypr/laptop.conf"
+fi
+
+if [ "$HYPR_SHARED_PLATFORM_CONF" == "empty" ]; then
+  echo "Platform config not set, available options: --desktop --laptop"
+  exit -1
+fi
+
+ln -s "$HYPR_SHARED_PLATFORM_CONF" "$SCRIPTPATH/hypr/shared.conf" || { echo "Warn: symlink $SCRIPTPATH/hypr/shared.conf already exist"; }
 
 if [ -d "$HYPERLAND_CONFIG_DIR" ] || [ -L "$HYPERLAND_CONFIG_DIR" ]; then
   echo "Warn: directory $HYPERLAND_CONFIG_DIR already exist; not copying config"
